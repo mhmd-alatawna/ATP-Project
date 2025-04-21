d86617c1b378436abadf332661ad315e23a799ef
mohammad <mohammadalatawna111@gmail.com>
2025-04-21 23:40:12 +0300

    made generating maze3D much faster

--- src/algorithms/maze3D/MyMaze3DGenerator.java
+++ src/algorithms/maze3D/MyMaze3DGenerator.java
@@ -1,6 +1,7 @@
 package algorithms.maze3D;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MyMaze3DGenerator extends AMaze3DGenerator{
     public MyMaze3DGenerator() {}
@@ -17,7 +18,7 @@ public class MyMaze3DGenerator extends AMaze3DGenerator{
         //make sure that start is 0
         result.getMatrix()[start.getDepthIndex()][start.getRowIndex()][start.getColumnIndex()] = 0 ;
 
-        primAlgoImplementation(result.getMatrix());
+        primAlgoImplementation(result.getMatrix() , start);
         //make sure that end is 0
         result.getMatrix()[end.getDepthIndex()][end.getRowIndex()][end.getColumnIndex()] = 0 ;
 
@@ -31,44 +32,41 @@ public class MyMaze3DGenerator extends AMaze3DGenerator{
         return result;
     }
 
-    private void primAlgoImplementation(int[][][] matrix) {
+    private void primAlgoImplementation(int[][][] matrix , Position3D start) {
         ArrayList<Position3D> yetToHandle = new ArrayList<>();
-        addNeighbours(matrix , yetToHandle , 0 ,0 , 0);
+        HashMap<String , Boolean> map = new HashMap<>() ;
+        map.put(start.toString() ,Boolean.TRUE) ;
+        addNeighbours(matrix , yetToHandle , map ,0 ,0 , 0);
         while(!yetToHandle.isEmpty()){
             int randomIndex = (int) (Math.random() * yetToHandle.size());
             Position3D p = yetToHandle.remove(randomIndex);
             int d = p.getDepthIndex() , r = p.getRowIndex() , c = p.getColumnIndex() ;
             matrix[d][r][c] = 0;
             findGoodNeighbour(matrix, d, r, c);
-            addNeighbours(matrix, yetToHandle , d , r, c);
+            addNeighbours(matrix, yetToHandle ,map , d , r, c);
         }
     }
 
-    private void addNeighbours(int[][][] matrix , ArrayList<Position3D> yetToHandle , int d , int r, int c){
+    private void addNeighbours(int[][][] matrix , ArrayList<Position3D> yetToHandle , HashMap<String , Boolean> map, int d , int r, int c){
         if(r + 2 < matrix[0].length && matrix[d][r + 2][c] == 1)
-            addWithoutDuplicate(yetToHandle , new Position3D(d,r + 2, c)) ;
+            addWithoutDuplicate(yetToHandle , map ,new Position3D(d,r + 2, c)) ;
         if(r - 2 >= 0 && matrix[d][r - 2][c] == 1)
-            addWithoutDuplicate(yetToHandle , new Position3D(d,r - 2, c)) ;
+            addWithoutDuplicate(yetToHandle , map ,new Position3D(d,r - 2, c)) ;
         if(c + 2 < matrix[0][0].length && matrix[d][r][c + 2] == 1)
-            addWithoutDuplicate(yetToHandle , new Position3D(d,r, c + 2)) ;
+            addWithoutDuplicate(yetToHandle ,map , new Position3D(d,r, c + 2)) ;
         if(c - 2 >= 0 && matrix[d][r][c - 2] == 1)
-            addWithoutDuplicate(yetToHandle , new Position3D(d,r, c - 2)) ;
+            addWithoutDuplicate(yetToHandle , map ,new Position3D(d,r, c - 2)) ;
         if(d + 2 < matrix.length && matrix[d + 2][r][c] == 1)
-            addWithoutDuplicate(yetToHandle , new Position3D(d + 2,r, c)) ;
+            addWithoutDuplicate(yetToHandle , map , new Position3D(d + 2,r, c)) ;
         if(d - 2 >= 0 && matrix[d - 2][r][c] == 1)
-            addWithoutDuplicate(yetToHandle , new Position3D(d - 2,r, c)) ;
+            addWithoutDuplicate(yetToHandle , map ,new Position3D(d - 2,r, c)) ;
     }
 
-    private void addWithoutDuplicate(ArrayList<Position3D> yetToHandle , Position3D toAdd){
-        boolean found = false ;
-        for(Position3D p : yetToHandle){
-            if (p.equals(toAdd)) {
-                found = true;
-                break;
-            }
-        }
-        if(!found)
+    private void addWithoutDuplicate(ArrayList<Position3D> yetToHandle , HashMap<String , Boolean> map , Position3D toAdd){
+        if(map.get(toAdd.toString()) != Boolean.TRUE){
+            map.put(toAdd.toString() , Boolean.TRUE);
             yetToHandle.add(toAdd);
+        }
     }
 
     private void findGoodNeighbour(int[][][] matrix , int d , int r , int c){
